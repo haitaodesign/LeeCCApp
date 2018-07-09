2f169f332d6962c7d23da8f311d26befd45c959c
lihaitao <1775814590@qq.com>
2018-07-09 21:50:17 +0800

    add statusBar

--- App/components/Container/Container.js
+++ App/components/Container/Container.js
@@ -11,7 +11,7 @@ const Container = ({ children }) => (
 )
 
 Container.propTypes = {
-  children: PropTypes.element
+  children: PropTypes.any
 }
 
 export default Container
--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 
 import { Container } from '../components/Container'
 
 export default () => (
   <Container>
+    <StatusBar translucent={false} barStyle="default"/>
     <View />
   </Container>
 )
