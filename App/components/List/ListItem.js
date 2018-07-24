310dd3e8f7b9ea2f1a1bd697351b3a66016660d3
lihaitao <1775814590@qq.com>
2018-07-24 22:02:19 +0800

    add options icons

--- App/components/List/ListItem.js
+++ App/components/List/ListItem.js
@@ -5,12 +5,13 @@ import { View, Text, TouchableHighlight } from 'react-native'
 import styles from './styles'
 import Icon from './Icon'
 
-const ListItem = ({text, onPress, checkmark = true, selected = false, visible = true}) => {
+const ListItem = ({text, onPress, checkmark = true, selected = false, visible = true, customIcon = null}) => {
   return (
     <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
       <View style={styles.row}>
         <Text style={styles.text}>{text}</Text>
         {selected ? <Icon visible={visible} checkmark={checkmark}/> : <Icon/>}
+        {customIcon}
       </View>
     </TouchableHighlight>
   )
@@ -20,7 +21,8 @@ ListItem.propTypes = {
   onPress: PropTypes.func,
   checkmark: PropTypes.bool,
   selected: PropTypes.bool,
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  customIcon: PropTypes.element
 }
 
 export default ListItem
---
+++ App/screens/Options.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { ScrollView, StatusBar, Platform } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { ListItem, Separator } from '../components/List'
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md'
+const ICON_COLOR = '#868686'
+const ICON_SIZE = 23
+export default class Options extends Component {
+  _handlePressThemes () {
+  }
+  _handlePressSite () {
+  }
+  render () {
+    return (
+      <ScrollView>
+        <StatusBar translucent={false} barStyle="default"/>
+        <ListItem text="Themes"
+          onPress={this._handlePressThemes.bind(this)}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} size={ICON_SIZE} color={ICON_COLOR}/>
+          }/>
+        <Separator />
+        <ListItem text="Fixer.io"
+          onPress={this._handlePressSite.bind(this)}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-link`} size={ICON_SIZE} color={ICON_COLOR}/>
+          }/>
+      </ScrollView>
+    )
+  }
+}
