5aa08bcde640360ebcf3275f1537ad19dd098606
lihaitao <15393243613@163.com>
2018-08-11 18:15:01 +0800

    add lint index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { AppRegistry } from "react-native";
-import App from "./src";
+import { AppRegistry } from 'react-native'
+import App from './src'
 
-import { name as appName } from "./app.json";
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
